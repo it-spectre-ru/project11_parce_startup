ab9e41f0621325869c1d6dd03804d7041750ea08
te <e>
2022-06-27 12:58:11 +0500

    Hrm. The Wayback Machine has not archived that URL

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-l = []
+from email import header
+from wsgiref import headers
+import requests
+from bs4 import BeautifulSoup
 
-for i in range(1, 200):
-  if (i%2!=0):
-    print(i)
+def get_data(url):
+  headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+  }
+
+  req = requests.get(url, headers)
+
+  with open('project.html', 'w') as file:
+    file.write(req.text)
+
+get_data('http://web.archive.org/web/20200712235623/http://www.edutainme.ru:80/edindex')
